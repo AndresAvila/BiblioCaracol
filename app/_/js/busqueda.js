40feffad8867a4bab636407ac71b331a700673ac
Lowdive <elisantiago.4e@gmail.com>
2014-11-15 13:58:33 -0600

    Added semicolon
    Pillaba el grunt que le faltaba semicolon a esto

--- app/_/js/busqueda.js
+++ app/_/js/busqueda.js
@@ -1,7 +1,7 @@
 function addSearchParam() {
 	var r = $($('#searchParams .row')[0]).clone();
 	var e = $($('#searchParams .row')[1]).clone();
-	r.find('.searchParamText').val("")
+	r.find('.searchParamText').val("");
 	$('#searchParams').append(r);
 	$('#searchParams').append(e);
 	$('.addParam').off('click');
