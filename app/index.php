afbda74ff7a8e7eacc85711ac76d5822cf77b13f
AndresAvila <andres.amdc@gmail.com>
2014-11-17 17:45:43 -0600

    Acentos
    Errores Ortográficos

--- app/index.php
+++ app/index.php
@@ -16,7 +16,7 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-md-6 col-md-offset-3 text-center">
-					<h1>Busqueda</h1>
+					<h1>Búsqueda</h1>
 				</div>
 			</div>
 			<form action="/busqueda" method="post">
