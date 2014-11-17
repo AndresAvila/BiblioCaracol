d798beabade1e437b83233bcb2a5bf3f635aef45
Ktl_XV <jorge@ktlxv.com>
2014-11-17 10:51:40 -0600

    Centrar "Busqueda"

--- app/index.php
+++ app/index.php
@@ -15,7 +15,7 @@
 	<div id="PageBody">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-6 col-md-offset-3">
+				<div class="col-md-6 col-md-offset-3 text-center">
 					<h1>Busqueda</h1>
 				</div>
 			</div>
