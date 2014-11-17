8854c23aaf4ce221e374054235c37f83918c1d21
Ktl_XV <jorge@ktlxv.com>
2014-11-17 15:58:47 -0600

    Corregir crear usuario

--- app/_/inc/nuevoUsuario.php
+++ app/_/inc/nuevoUsuario.php
@@ -2,5 +2,9 @@
 	$username = checkInput($_POST['username']);
 	$mail = checkInput($_POST['mail']);
 	$pass = password_hash(checkInput($_POST['password']),PASSWORD_DEFAULT);
-	//echo "INSERT INTO Usuarios (NULL, '$username', '$mail', '$pass')";
-	$query = $db->query("INSERT INTO Usuarios (NULL, '$username', '$mail', '$pass')");
+	$query = $db->query("INSERT INTO Usuarios (Nombre, Mail, Contrasena) VALUES ('$username', '$mail', '$pass')");
+
+	if($query){
+		$_SESSION['uid'] = $db->insert_id;
+		$_SESSION['username'] = $username;
+	}
