4a2b1607e3ba9767324806cae5b1be55d6defc2b
Lowdive <elisantiago.4e@gmail.com>
2014-11-19 06:53:59 -0600

    pedidos
    push pedidos

--- app/realizapedido.php
+++ app/realizapedido.php
@@ -1,68 +1,117 @@
 <?php require($_SERVER['DOCUMENT_ROOT'] . '/_/inc/init.php'); ?>
 <!doctype html>
-<!--[if lt IE 7]>      <html class="no-js ie ie6 lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js ie ie7 lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js ie ie8 lt-ie9"> <![endif]-->
-<!--[if IE 9]>         <html class="no-js ie ie9 lt-ie10"> <![endif]-->
+<!--[if lt IE 7]>			<html class="no-js ie ie6 lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>				<html class="no-js ie ie7 lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>				<html class="no-js ie ie8 lt-ie9"> <![endif]-->
+<!--[if IE 9]>				<html class="no-js ie ie9 lt-ie10"> <![endif]-->
 <!--[if gt IE 9]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
-    <title></title>
-    <meta name="description" content="">
-    <?php require("_/inc/head.php"); ?>
-	<!-- build:css({.tmp,app}) /_/css/table.css -->
-		<link rel="stylesheet" href="/_/css/table.css">
-	<!-- endbuild -->
+		<title>Centro Cultural Caracol</title>
+		<meta name="description" content="">
+		<?php require("_/inc/head.php"); ?>
+
 </head>
 <body>
 	<?php require('_/inc/header.php'); ?>
 	<div id="PageBody">
+		<?php
+			if(isset($_POST['titulo']))
+				require_once('realizapedidoDBA.php');
+			else{ ?>
 		<div class="container">
 			<div class="row">
 				<div class="col-md-6 col-md-offset-3 text-center">
-					<h1>Realizar Pedido</h1>
+					<h1>Solicitud de Contenido Nuevo</h1>
 				</div>
 			</div>
-			<form action="/pedidos" method="post">
-				<div id="searchParams">
-					<div class="row">
-						<div class="col-md-2 col-md-offset-2">
-							<select class="form-control">
-								<option>Todo</option>
-								<option>Titulo</option>
-								<option>Autor</option>
-								<option>Tema</option>
-								<option>Genero</option>
-							</select>
-						</div>
-						<div class="col-md-5">
-								<input type="text" class="form-control searchParamText">
-						</div><!-- /.col-lg-6 -->
-						<div class="col-md-1">
-							<button type="button" class="addParam btn btn-success"><span class="glyphicon glyphicon-plus"></span></button>
-						</div>
-					</div>
-					<div class="row">&nbsp;</div>
+			<div class="row">
+				<div class="col-md-6 col-md-offset-3 text-center">
+					<img id="portada" src="">
 				</div>
-				<div class="row text-center">
-					<button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-search" aria-hidden="true"></span> Buscar</button>
+			</div>
+			<form role="form" method="post" >
+				<div class="form-group">
+					<label for="upc">UPC / ISBN</label>
+					<input type="text" class="form-control" name="upc" id="upc" >
+					<p class="help-block">Esta información aparece debajo del código de barras.</p>
 				</div>
+				<div class="form-group">
+					<label for="tipo">Tipo de contenido</label>
+					<select class="form-control" name="tipo" id="tipo" >
+						<option>Libro</option>
+						<option>Revista</option>
+						<option>Video</option>
+					</select>
+				</div>
+				<div class="form-group">
+					<label for="titulo">Titulo</label>
+					<input type="text" class="form-control" name="titulo" id="titulo" >
+				</div>
+				<div class="form-group">
+					<label for="autores">Autores</label>
+					<input type="text" class="form-control" name="autores" id="autores" >
+					<p class="help-block">Cada autor debe de ir separado por una coma.</p>
+				</div>
+				<div class="form-group">
+					<label for="edad">Edades</label>
+					<select class="form-control" name="edad" >
+						<option>Niños</option>
+						<option>Jóvenes</option>
+						<option>Adultos</option>
+					</select>
+					<p class="help-block">El púlico principal para este contenido.</p>
+				</div>
+				<div class="form-group">
+					<label for="temas">Temas</label>
+					<input type="text" name="temas" class="form-control" >
+					<p class="help-block">Cada tema debe de ir separado por una coma.</p>
+				</div>
+				<div class="form-group">
+					<label for="generos">Generos</label>
+					<input type="text" name="generos" class="form-control" >
+					<p class="help-block">Cada generos debe de ir separado por una coma.</p>
+				</div>
+				<div class="form-group">
+					<label for="editorial">Editorial</label>
+					<input type="text" class="form-control" name="editorial" id="editorial" >
+				</div>
+				<div class="form-group">
+					<label for="fechaPublicacion">Fecha de Publicación</label>
+					<input type="text" class="form-control" name="fechaPublicacion" id="fechaPublicacion" >
+				</div>
+				<div class="form-group">
+					<label for="idioma">Idioma</label>
+					<input type="text" class="form-control" name="idioma" id="idioma" >
+				</div>
+				<div class="form-group">
+					<label for="portada">Portada</label>
+					<input type="text" class="form-control" name="portada" id="portadaUrl" >
+					<p class="help-block">Dirección de la imagen de la portada.</p>
+				</div>
+				<div class="checkbox">
+					<label>
+						<input type="checkbox" name="grande"> Grande
+					</label>
+				</div>
+				<button type="submit" class="btn btn-default">Pedir Contenido Nuevo</button>
 			</form>
 		</div>
+		<?php } ?>
+		<!--[if lt IE 9]>
+				<p class="chromeframe">You are using an <strong>outdated</strong> browser.
+				Please <a href="http://browsehappy.com/">upgrade your browser</a>
+				or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+		<![endif]-->
 
-    <!--[if lt IE 9]>
-        <p class="chromeframe">You are using an <strong>outdated</strong> browser. 
-        Please <a href="http://browsehappy.com/">upgrade your browser</a> 
-        or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
-    <![endif]-->
+		<?php require('_/inc/footer.php'); ?>
 
-    <?php require('_/inc/footer.php'); ?>
+		<!-- JAVASCRIPT -->
+		<?php require('_/inc/analytics.php'); ?>
 
-    <!-- JAVASCRIPT -->
-    <?php require('_/inc/analytics.php'); ?>
-		
-    <?php require('_/inc/tail.php'); ?>
-	<!-- build:js /_/js/index.js -->
-		<script src="/_/js/busqueda.js"></script>
+		<?php require('_/inc/tail.php'); ?>
+	<!-- build:js /_/js/nuevoContenido.js -->
+		<script type="text/javascript" src="/_/js/autofillISBN.js"></script>
 	<!-- endbuild -->
 </body>
 </html>
+
---
+++ app/realizapedidoDBA.php
@@ -0,0 +1,65 @@
+<?php
+
+$autores = array_map('trim', explode(",", checkInput($_POST['autores'])));
+$autoresId = array();
+foreach($autores as $nombreAutor){
+	$queryAutorExistente = $db->query("SELECT idAutor FROM Autores WHERE Nombre = '$nombreAutor'");
+	if($queryAutorExistente->num_rows == 1)
+		$autoresId[] = $queryAutorExistente->fetch_object()->idAutor;
+	else{
+		$queryAutor = $db->query("INSERT INTO Autores (Nombre) VALUES ('$nombreAutor')");
+		$autoresId[] = $db->insert_id;
+	}
+}
+
+$generos = array_map('trim', explode(",", checkInput($_POST['generos'])));
+$generosId = array();
+foreach($generos as $nombreGenero){
+	$queryGeneroExistente = $db->query("SELECT idGenero FROM Generos WHERE Nombre = '$nombreGenero'");
+	if($queryGeneroExistente->num_rows == 1)
+		$generosId[] = $queryGeneroExistente->fetch_object()->idGenero;
+	else{
+		$queryGenero = $db->query("INSERT INTO Generos (Nombre) VALUES ('$nombreGenero')");
+		$generosId[] = $db->insert_id;
+	}
+}
+
+$temas = array_map('trim', explode(",", checkInput($_POST['temas'])));
+$temasId = array();
+foreach($temas as $nombreTema){
+	$queryTemaExistente = $db->query("SELECT idTema FROM Temas WHERE Nombre = '$nombreTema'");
+	if($queryTemaExistente->num_rows == 1)
+		$temasId[] = $queryTemaExistente->fetch_object()->idTema;
+	else{
+		$queryTema = $db->query("INSERT INTO Temas (Nombre) VALUES ('$nombreTema')");
+		$temasId[] = $db->insert_id;
+	}
+}
+
+$query = $db->query('INSERT INTO Contenido (Nombre, Tipo, UPC, Editorial, Idioma, FechaPublicacion, PublicoMeta, URLPortada, Grande) VALUES('
+	.'\''.checkInput($_POST['titulo']).'\','
+	.'\''.checkInput($_POST['tipo']).'\','
+	.'\''.checkInput($_POST['upc']).'\','
+	.'\''.checkInput($_POST['editorial']).'\','
+	.'\''.checkInput($_POST['idioma']).'\','
+	.'\''.checkInput($_POST['fechaPublicacion']).'\','
+	.'\''.checkInput($_POST['edad']).'\','
+	.'\''.checkInput($_POST['portada']).'\','
+	.'\''.(isset($_POST['grande'])?1:0).'\')');
+
+if($query){
+	$idContenido = $db->insert_id;
+	foreach($autoresId as $idAutor)
+		$query = $db->query("INSERT INTO Autores_has_Contenido (Autores_idAutor, Contenido_idContenido) VALUES ('$idAutor', '$idContenido')");
+	foreach($generosId as $idGenero)
+		$query = $db->query("INSERT INTO Contenido_has_Generos (Generos_idGenero, Contenido_idContenido) VALUES ('$idGenero', '$idContenido')");
+	foreach($temasId as $idTema)
+		$query = $db->query("INSERT INTO Contenido_has_Temas (Temas_idTema, Contenido_idContenido) VALUES ('$idTema', '$idContenido')");
+	
+	echo 'Pedido realizado';
+}else if($db->errno == 1062){
+	$queryLibroExistente = $db->query("SELECT idContenido, Nombre FROM Contenido WHERE UPC = '".checkInput($_POST['upc'])."'");
+	$res = $queryLibroExistente->fetch_object();
+
+	echo $res->Nombre.' ya existe, ¿deseas <a href="/admin/editarContenido?idContenido='.$res->idContenido.'">editarlo</a>?';
+}
