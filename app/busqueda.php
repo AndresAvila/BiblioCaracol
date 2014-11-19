ad9861b5a4a81881d9373501082467d10c3223f8
Lowdive <elisantiago.4e@gmail.com>
2014-11-18 18:59:37 -0600

    Query de autor que funciona para busqueda
    primer intento de búsqueda

--- app/busqueda.php
+++ app/busqueda.php
@@ -27,7 +27,7 @@
 				<th>Fecha de Publicación</th>
 			</tr>
 		<?php
-			$query = $db->query("SELECT Nombre, (select a.Nombre from Autores a where a.idAutor = Contenido.Autores_idAutor) as Autor, Tipo, Editorial, UPC, Idioma, FechaPublicacion FROM Contenido");
+			$query = $db->query('select * from (SELECT Nombre, (select a.Nombre from Autores a where a.idAutor = Contenido.Autores_idAutor) as Autor, Tipo, Editorial, UPC, Idioma, FechaPublicacion FROM Contenido) o where o.Autor like "%'.checkInput($_POST['texto1']).'%"');
 			while($res = $query->fetch_object()){ ?>
 				<tr>
 					<td><?=$res->Nombre;?></td>
