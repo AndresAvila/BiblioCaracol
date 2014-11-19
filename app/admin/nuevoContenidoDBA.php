215f2de4770f35c341990c511d1f747510048cf7
Ktl_XV <jorge@ktlxv.com>
2014-11-18 19:19:01 -0600

    Evitar dupicados al crear contenido

--- app/admin/nuevoContenidoDBA.php
+++ app/admin/nuevoContenidoDBA.php
@@ -1,16 +1,20 @@
 <?php
 
-$queryAutorExistente = $db->query("SELECT idAutor FROM Autores WHERE Nombre = '".checkInput($_POST['autores'])."'");
-if($queryAutorExistente->num_rows == 1)
-	$autor = $queryAutorExistente->fetch_object()->idAutor;
-else{
-	$queryAutor = $db->query("INSERT INTO Autores (Nombre) VALUES ('".checkInput($_POST['autores'])."')");
-	$autor = $db->insert_id;
+$autores = explode(", ",checkInput($_POST['autores']));
+$autoresId = array();
+
+foreach($autores as $nombreAutor){
+	$queryAutorExistente = $db->query("SELECT idAutor FROM Autores WHERE Nombre = '$nombreAutor'");
+	if($queryAutorExistente->num_rows == 1)
+		$autoresId[] = $queryAutorExistente->fetch_object()->idAutor;
+	else{
+		$queryAutor = $db->query("INSERT INTO Autores (Nombre) VALUES ('$nombreAutor')");
+		$autoresId[] = $db->insert_id;
+	}
 }
 
-$query = $db->query('INSERT INTO Contenido (Nombre, Autores_idAutor, Tipo, UPC, Editorial, Idioma, FechaPublicacion, PublicoMeta, URLPortada, Grande) VALUES('
+$query = $db->query('INSERT INTO Contenido (Nombre, Tipo, UPC, Editorial, Idioma, FechaPublicacion, PublicoMeta, URLPortada, Grande) VALUES('
 	.'\''.checkInput($_POST['titulo']).'\','
-	.'\''.$autor.'\','
 	.'\''.checkInput($_POST['tipo']).'\','
 	.'\''.checkInput($_POST['upc']).'\','
 	.'\''.checkInput($_POST['editorial']).'\','
@@ -20,5 +24,15 @@ $query = $db->query('INSERT INTO Contenido (Nombre, Autores_idAutor, Tipo, UPC, 
 	.'\''.checkInput($_POST['portada']).'\','
 	.'\''.(isset($_POST['grande'])?1:0).'\')');
 
-if($query)
+if($query){
+	$idContenido = $db->insert_id;
+	foreach($autoresId as $idAutor)
+		$query = $db->query("INSERT INTO Autores_has_Contenido (Autores_idAutor, Contenido_idContenido) VALUES ('$idAutor', '$idContenido')");
+	
 	echo 'Contenido agregado correctamente';
+}else if($db->errno == 1062){
+	$queryLibroExistente = $db->query("SELECT idContenido, Nombre FROM Contenido WHERE UPC = '".checkInput($_POST['upc'])."'");
+	$res = $queryLibroExistente->fetch_object();
+
+	echo $res->Nombre.' ya existe, ¿deseas <a href="/admin/editarContenido?idContenido='.$res->idContenido.'">editarlo</a>?';
+}
