29123611cd11fb6609fc5b43a12d384d7b563fb4
Ktl_XV <jorge@ktlxv.com>
2014-11-26 08:05:22 -0600

    No need for /index

--- app/_/inc/header.php
+++ app/_/inc/header.php
@@ -78,7 +78,7 @@
 			<!-- Collect the nav links, forms, and other content for toggling -->
 			<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 				<ul class="nav navbar-nav">
-					<li><a href="/index"><span class="glyphicon glyphicon-search" aria-hidden="true"></span> Búsqueda</a></li>
+					<li><a href="/"><span class="glyphicon glyphicon-search" aria-hidden="true"></span> Búsqueda</a></li>
 					<?php if(isset($_SESSION['uid'])) {?>
 						<li><a href="/rentas"><span class="glyphicon glyphicon-briefcase" aria-hidden="true"></span> Rentas</a></li>
 						<li><a href="/realizapedido"><span class="glyphicon glyphicon-envelope" aria-hidden="true"></span> Pedidos</a></li>
