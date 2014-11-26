d62191d4fe0434fb3d651af9799746a2ef03200b
Ktl_XV <jorge@ktlxv.com>
2014-11-26 10:00:30 -0600

    Arreglar footer

--- app/_/inc/footer.php
+++ app/_/inc/footer.php
@@ -1,3 +1,7 @@
-<footer id="SiteFooter" class="footer navbar-fixed-bottom center text-center">
+<div id="push"></div>
+</div>
+</div>
+<footer id="SiteFooter" class="footer center text-center">
+	<br />
 	Centro Cultural Caracol   &copy; 2013-<?php echo date('Y'); ?>
 </footer>
